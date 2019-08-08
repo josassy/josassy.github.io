c57a859462ffe949347cdeb44a102d570418faa1
Josiah Lansford <jlansford@radiancetech.com>
2019-08-08 14:32:54 -0400

    restarted from scratch, added new logo

--- launch server.bat
+++ launch server.bat
@@ -1,3 +1,3 @@
-echo "starting a world of happiness!!"
+echo "Launching localhost server..."
 start python -m http.server
 "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" http://localhost:8000/
