ada148f481f5d22c54b2d79246ee112b19c89070
Josiah Lansford <redrobbin99@gmail.com>
2019-08-21 09:43:55 -0400

    use state to update class, hamburger disappears on desktop

--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import logo from './images/jl-logo-white-noborders.svg';
+import HamburgerMenu from 'react-hamburger-menu';
 
 class Header extends React.Component {
   constructor() {
     super();
     this.state = {
+      isOpen: false,
       width: window.innerWidth,
     };
   }
@@ -20,41 +22,34 @@ class Header extends React.Component {
     this.setState({ width: window.innerWidth });
   };
 
+  handleClick() {
+    console.log("a click was handled");
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+
   render() {
     const { width }  = this.state;
     const isMobile = width <= 600;
 
-    if (isMobile) {
-      return (
-        <header>
-          <nav className="mobile">
-
-            <a href="/"><img src={logo} alt="" id="header-logo"/></a>
-            <ul>
-              <li><a href="/">Home</a></li>
-              <li><a href="/">Experience</a></li>
-              <li><a href="/">Education</a></li>
-              <li><a href="/">Contact</a></li>
-            </ul>
-          </nav>
-        </header>
-      )
-    }
-    else {
-      return (
-        <header>
-          <nav>
-            <a href="/"><img src={logo} alt="" /></a>
-            <ul>
-              <li><a href="/">Home</a></li>
-              <li><a href="/">Experience</a></li>
-              <li><a href="/">Education</a></li>
-              <li><a href="/">Contact</a></li>
-            </ul>
-          </nav>
-        </header>
-      )
-    }
+    return (
+      <header>
+      <nav className={ isMobile ? "mobile" : "desktop" }>
+          <a href="/"><img src={logo} alt="" id="header-logo"/></a>
+          <HamburgerMenu
+            isOpen={this.state.isOpen}
+            menuClicked={this.handleClick.bind(this)}
+          />
+          <ul className={ this.state.isOpen ? "active" : "" }>
+            <li><a href="/">Home</a></li>
+            <li><a href="/">Experience</a></li>
+            <li><a href="/">Education</a></li>
+            <li><a href="/">Contact</a></li>
+          </ul>
+        </nav>
+      </header>
+    )
   }
 }
 
