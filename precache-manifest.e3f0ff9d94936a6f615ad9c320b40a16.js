0b372a99e721b8a9d87490ad96789dde899c6860
Josiah Lansford <redrobbin99@gmail.com>
2019-09-10 10:40:40 -0400

    Updates

---
+++ precache-manifest.e3f0ff9d94936a6f615ad9c320b40a16.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48a8bcbda3267b665191ec44e296ff47",
+    "url": "/index.html"
+  },
+  {
+    "revision": "09b7eb8b403e6573cfd4",
+    "url": "/static/css/main.89f8a9d4.chunk.css"
+  },
+  {
+    "revision": "259944e2c24c384efbca",
+    "url": "/static/js/2.f0259827.chunk.js"
+  },
+  {
+    "revision": "09b7eb8b403e6573cfd4",
+    "url": "/static/js/main.e958d404.chunk.js"
+  },
+  {
+    "revision": "981a86c32144eb9970e4",
+    "url": "/static/js/runtime~main.efca4bcc.js"
+  },
+  {
+    "revision": "eafa4a5122a90c8398dce7cbfa5a602f",
+    "url": "/static/media/jl-logo-white-noborders.eafa4a51.svg"
+  }
+]);
