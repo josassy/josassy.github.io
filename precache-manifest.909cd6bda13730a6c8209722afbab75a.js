1c8ad8c08f29a829cdb5dcc65c6254eab60ce8c8
Josiah Lansford <redrobbin99@gmail.com>
2019-09-10 09:46:33 -0400

    Updates

---
+++ precache-manifest.909cd6bda13730a6c8209722afbab75a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "702574f2e3b69b30345a085a94d7780b",
+    "url": "/josassy.github.io/index.html"
+  },
+  {
+    "revision": "522cce35d1355174e31f",
+    "url": "/josassy.github.io/static/css/main.89f8a9d4.chunk.css"
+  },
+  {
+    "revision": "259944e2c24c384efbca",
+    "url": "/josassy.github.io/static/js/2.f0259827.chunk.js"
+  },
+  {
+    "revision": "522cce35d1355174e31f",
+    "url": "/josassy.github.io/static/js/main.00a08c91.chunk.js"
+  },
+  {
+    "revision": "3f93408fb0e59060cd34",
+    "url": "/josassy.github.io/static/js/runtime~main.8d00278e.js"
+  },
+  {
+    "revision": "eafa4a5122a90c8398dce7cbfa5a602f",
+    "url": "/josassy.github.io/static/media/jl-logo-white-noborders.eafa4a51.svg"
+  }
+]);
