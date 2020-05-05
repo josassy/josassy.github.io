6b372225fc8cdebbc47bd2a994f00fb7c2aba9bb
Josiah Lansford <redrobbin99@gmail.com>
2020-05-04 21:29:54 -0400

    fixed up projects image scaling

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -5,15 +5,17 @@ export const Projects = () => {
   return (
     <>
       <h1>Projects</h1>
-      {projectsData.map(item => (
-      <a key={item.url} href={item.url}>
+      {projectsData.map(({title, description, date, url, src}) => (
+      <a key={url} href={url}>
         <div className="card">
-
-          <img src={item["image-url"]} alt={`Screenshot of ${item.title}`}/>
-          <div>
-            <h2>{item.title}</h2>
-            <p>{item.description}</p>
-            <p className="caption">{item.date}</p>
+          {/* workaround for Chrome flex image bug: https://www.freecodecamp.org/forum/t/flex-basis-wont-shrink-image-in-flexbox/255701*/}
+          <div className="image-container">
+            <img src={src} alt={`Screenshot of ${title}`}/>
+          </div>
+          <div className="description">
+            <h2>{title}</h2>
+            <p>{description}</p>
+            <p className="caption">{date}</p>
           </div>
         </div>
       </a>
