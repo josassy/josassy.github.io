9180438510264fb6ef3dc9307406ecc220c4f214
Josiah Lansford <39782615+josassy@users.noreply.github.com>
2021-02-19 00:12:58 -0500

    add "rocket oblivion" to projects page

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -8,7 +8,7 @@ export const Projects = () => {
         <h2>Projects</h2>
         <ProjectCard
           title="GameStonks"
-          description="Beat your opponent in 2-player local play to make the most money from GameStonks stonk! Built in 48 hours for the 2021 Global Game Jam."
+          description="Beat your opponent in 2-player local play to make the most money from GameStonks stonk! Built with Unreal Engine 4 in 48 hours for the 2021 Global Game Jam."
           date="January 2021"
           url="https://josassy.itch.io/gamestonks"
           src="/images/projects/gamestonks/1.png"
@@ -23,15 +23,13 @@ export const Projects = () => {
           description="Web-based check-in kiosk built for Cedarville University's Information Technology department. Built using a React front-end and a Python Flask API, and integrated with existing TeamDynamix ticket-management system."
           date="August 2020"
           src="/images/projects/tdxservicecounter/1.png"
-          images={
-            [
-              "/images/projects/tdxservicecounter/1.png",
-              "/images/projects/tdxservicecounter/2.png",
-              "/images/projects/tdxservicecounter/3.png",
-              "/images/projects/tdxservicecounter/4.png",
-              "/images/projects/tdxservicecounter/5.png"
-            ]
-          }
+          images={[
+            "/images/projects/tdxservicecounter/1.png",
+            "/images/projects/tdxservicecounter/2.png",
+            "/images/projects/tdxservicecounter/3.png",
+            "/images/projects/tdxservicecounter/4.png",
+            "/images/projects/tdxservicecounter/5.png"
+          ]}
         />
         <ProjectCard
           title="React Cache Simulator"
@@ -39,6 +37,18 @@ export const Projects = () => {
           date="April 2020"
           url="https://josassy.github.io/cache-simulator"
           src="/images/projects/cache-simulator-800.png"
+        />
+        <ProjectCard
+          title="Rocket Oblivion"
+          description="Casual rocket-themed resource management game with an interactive soundtrack. Built with Godot Game Engine in 48 hours for the 2020 Global Game Jam."
+          date="February 2020"
+          url="https://globalgamejam.org/2020/games/rocket-oblivion-4"
+          src="/images/projects/rocketoblivion/1.png"
+          images={[
+            "/images/projects/rocketoblivion/1.png",
+            "/images/projects/rocketoblivion/2.png",
+            "/images/projects/rocketoblivion/3.png"
+          ]}
         />
       </div>
     </section>
