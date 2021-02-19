85fffe8240c16a5d09e9e8a3d41bf7aa0f96aa54
Josiah Lansford <39782615+josassy@users.noreply.github.com>
2021-02-18 22:48:13 -0500

    add gamestonks project

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -5,7 +5,7 @@ import 'react-image-lightbox/style.css'; // This only needs to be imported once 
 export const ProjectCard = ({
   title, description, date, url, src, action, images
 }) => {
-  
+
   const [modalOpen, setModalOpen] = useState(false);
   const [photoIndex, setPhotoIndex] = useState(0);
   const showModal = () => {
@@ -19,25 +19,29 @@ export const ProjectCard = ({
     <div key={url} className="card">
       {/* workaround for Chrome flex image bug: https://www.freecodecamp.org/forum/t/flex-basis-wont-shrink-image-in-flexbox/255701*/}
       <div className="image-container">
-        <img src={src} alt={`Screenshot of ${title}`}/>
+        <img src={src} alt={`Screenshot of ${title}`} />
       </div>
       <div className="description">
         <h3>{title}</h3>
         <p>{description}</p>
         <p className="caption">{date}</p>
-        <a href={url} onClick={showModal} className="caption action-link"><p>{action} &raquo;</p></a>
+        <div className="flexContainer action-link">
+          {url && <a href={url} className="caption"><p>View Site &raquo;</p></a>}
+          {images && <a onClick={showModal} className="caption"><p>View Gallery &raquo;</p></a>}
+        </div>
       </div>
       {(images && modalOpen) &&
-          <Lightbox
-            mainSrc={images[photoIndex]}
-            nextSrc={images[(photoIndex + 1) % images.length]}
-            prevSrc={images[(photoIndex + images.length - 1) % images.length]}
-            onCloseRequest={hideModal}
-            onMovePrevRequest={() => setPhotoIndex(
-              (photoIndex + images.length - 1) % images.length)}
-            onMoveNextRequest={() => setPhotoIndex(
-              (photoIndex + 1) % images.length)}
-          />
-        }
+        <Lightbox
+          mainSrc={images[photoIndex]}
+          nextSrc={images[(photoIndex + 1) % images.length]}
+          prevSrc={images[(photoIndex + images.length - 1) % images.length]}
+          onCloseRequest={hideModal}
+          onMovePrevRequest={() => setPhotoIndex(
+            (photoIndex + images.length - 1) % images.length)}
+          onMoveNextRequest={() => setPhotoIndex(
+            (photoIndex + 1) % images.length)}
+        />
+      }
     </div>
-)};
+  )
+};
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -6,12 +6,23 @@ export const Projects = () => {
     <section id="projects">
       <div className="section-content">
         <h2>Projects</h2>
-        <ProjectCard 
+        <ProjectCard
+          title="GameStonks"
+          description="Beat your opponent in 2-player local play to make the most money from GameStonks stonk! Built in 48 hours for the 2021 Global Game Jam."
+          date="January 2021"
+          url="https://josassy.itch.io/gamestonks"
+          src="/images/projects/gamestonks/1.png"
+          images={[
+            "/images/projects/gamestonks/1.png",
+            "/images/projects/gamestonks/2.png",
+            "/images/projects/gamestonks/3.png"
+          ]}
+        />
+        <ProjectCard
           title="TeamDynamix Service Counter Kiosk"
           description="Web-based check-in kiosk built for Cedarville University's Information Technology department. Built using a React front-end and a Python Flask API, and integrated with existing TeamDynamix ticket-management system."
           date="August 2020"
           src="/images/projects/tdxservicecounter/1.png"
-          action="View Gallery"
           images={
             [
               "/images/projects/tdxservicecounter/1.png",
@@ -21,16 +32,15 @@ export const Projects = () => {
               "/images/projects/tdxservicecounter/5.png"
             ]
           }
-          />
+        />
         <ProjectCard
           title="React Cache Simulator"
           description="Simple 2-way set associative cache simulator built using React. Created for Computer Architecture class at Cedarville University."
           date="April 2020"
           url="https://josassy.github.io/cache-simulator"
           src="/images/projects/cache-simulator-800.png"
-          action="View Site"
-          />
-        </div>
+        />
+      </div>
     </section>
   )
 }
